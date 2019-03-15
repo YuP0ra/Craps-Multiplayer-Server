c5a84c7ec4951c8aa597c89c260dc4df18529298
AbdElAziz Moftah <vb.promax@gmail.com>
2019-03-15 14:18:08 +0200

    ...

--- Requests/rooms.py
+++ Requests/rooms.py
@@ -1,14 +1,17 @@
 import json
 
 
-""" ############### Loading the settings json config file ############### """
-config_file = None
-with open('Statics/settings.json') as json_file:
+""" ############### Loading the rooms json config file ############### """
+configFile = None
+with open('Statics/roomsInfo.json') as json_file:
     config_file = json.load(json_file)
 
 
-""" ####################### Loading rooms info ########################## """
-rooms_active_players = [0] * len(config_file['rooms_name'])
+""" ####################### Initializing Rooms ####################### """
+crapsRooms = {}
+for room_name in config_file['rooms_name']:
+    crapsRooms[room_name] = []
+
 
 def getRoomsFullInfo():
     return {
@@ -24,3 +27,41 @@ def getRoomsActivePlayers():
             "TYPE"  : "ACTIVIY_ROOMS_INFO",
             "ACTIVE": config_file['active_players']
             }
+
+
+def ROOMS_FULL_INFO(client, request):
+    client.send(getRoomsFullInfo())
+
+
+def ROOMS_ACTIVITY_INFO(client, request):
+    client.send(getRoomsActivePlayers())
+
+
+def JOIN_ROOM_REQUEST(client, request):
+    roomsActivity = config_file['active_players']
+    roomIndex = configFile['rooms_name'].index(request['ROOM_NAME'])
+    playerTokenList, playerActiveList = crapsRooms[request['ROOM_NAME']]
+
+    if not client.TOKEN in playerTokenList:     #The player is not in the room
+        if roomsActivity[roomIndex] < 5:
+            playerTokenList.append(client.TOKEN)
+            playerActiveList.append(True)
+            roomsActivity[roomIndex] += 1
+    else:
+        index = playerTokenList.index(client.TOKEN)
+        playerActiveList[index] = True
+
+
+def LEAVE_ROOM_REQUEST(client, request):
+    roomsActivity = config_file['active_players']
+    roomIndex = configFile['rooms_name'].index(request['ROOM_NAME'])
+    playerTokenList, playerActiveList = crapsRooms[request['ROOM_NAME']]
+
+    if not client.TOKEN in playerTokenList:     #The player is not in the room
+        if roomsActivity[roomIndex] < 5:
+            playerTokenList.append(client.TOKEN)
+            playerActiveList.append(True)
+            roomsActivity[roomIndex] += 1
+    else:
+        index = playerTokenList.index(client.TOKEN)
+        playerActiveList[index] = True
